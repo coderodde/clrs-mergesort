0c2fe1ec5c39be4874b0700c66f0e233c258c405
Rodion Efremov <rodion.efremov@helsinki.fi>
2014-06-02 10:35:36 +0300

    More demo stuff.

--- src/net/coderodde/util/Demo.java
+++ src/net/coderodde/util/Demo.java
@@ -5,11 +5,12 @@ import java.util.Random;
 public class Demo {
  
     public static void main(String... args) {
-        final int SIZE = 10000000;
+        final int SIZE = 5000000;
         final long SEED = System.currentTimeMillis();
         final Random r = new Random(SEED);
         final Integer[] array1 = Utils.getRandomIntegerArray(SIZE, r);
         final Integer[] array2 = array1.clone();
+        final Integer[] array3 = array1.clone();
         
         System.out.println("Seed: " + SEED);
         
@@ -26,14 +27,26 @@ public class Demo {
         
         ta = System.currentTimeMillis();
         
-        Arrays.sort(array2);
+        java.util.Arrays.parallelSort(array2);
+        
+        tb = System.currentTimeMillis();
+        
+        System.out.println("JDK parallel sort: " + (tb - ta) + " ms, sorted: " +
+                Utils.isSorted(array2));
+        
+        ////
+        
+        ta = System.currentTimeMillis();
+        
+        Arrays.sort(array3);
         
         tb = System.currentTimeMillis();
         
         System.out.println("My sort: " + (tb - ta) + " ms, sorted: " +
-                Utils.isSorted(array2));
+                Utils.isSorted(array3));
         
         System.out.println("Arrays are equal: " + 
-                Utils.arraysEqual(array1, array2));
+                (Utils.arraysEqual(array1, array2) 
+                && Utils.arraysEqual(array1, array3)));
     }
 }
